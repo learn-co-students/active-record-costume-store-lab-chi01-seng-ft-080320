87c9be0013b6addeec8438d795bf49febf0d3073
Baguette-boi <vmanzanilla7@gmail.com>
2020-08-16 18:33:01 -0500

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,5 @@
-# Create your Costume class here
-# It should inherit from ActiveRecord::Base
+class Costume < ActiveRecord::Base
+
+    Costume.create(name: "Unisex Adult Hot Dog costume")
+    Costume.create(name: "Happy Penguin", image_url: "http://img.costumecraze.com/images/vendors/rasta/7139-large.jpg")
+end
